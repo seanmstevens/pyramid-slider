8ea17577dcf52a4f9a57bb5c62af17edb7bd48e6
Sean Stevens <sstevens88@gmail.com>
2017-07-31 18:47:32 -0700

    Finished HTML pyramid generator

--- mario.js
+++ mario.js
@@ -1,5 +1,6 @@
 
-printPyramid(15);
+printPyramid(9);
+drawPyramid(9);
 
 
 /*
@@ -14,11 +15,24 @@ printPyramid(15);
  *      ######
  */
 function printPyramid(height) {
-    
     for (let i = 0; i < height; i++) {
         var bricks = '#'.repeat(i + 2);
         var spaces = ' '.repeat(height - i - 1);
         console.log(spaces + bricks);
     }
-
 }
+
+function drawPyramid(height) {
+    for (let i = 2; i < height + 2; i++) {
+        var bricks = '#'.repeat(i);
+        var spaces = '\u00A0'.repeat(1 + height - i);
+        var child = document.createElement('p');
+        child.textContent = spaces + bricks;
+        document.getElementById('pyramid').appendChild(child);
+    }
+}
+
+var pyramid = document.getElementById('pyramid');
+var construction = document.getElementById('construction');
+
+pyramid.removeChild(construction);
